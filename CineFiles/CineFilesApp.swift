de484e6b63783895dddd633b172ee23cb54c3d79
Longhi <72895064+longhirar@users.noreply.github.com>
2025-05-28 11:41:44 -0300

    Initial Commit

---
+++ CineFiles/CineFilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineFilesApp.swift
+//  CineFiles
+//
+//  Created by Longhi on 28/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CineFilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
