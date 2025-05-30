97deed56e368604c40e3a31191a405b274feeb95
pollisdr <contato.pollianasdr@gmail.com>
2025-05-30 11:30:36 -0300

    cappa perfil

--- CineFiles/View/ProfileView.swift
+++ CineFiles/View/ProfileView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+        
+        ZStack{
+            
+            VStack {
+                Image("capa-perfil")
+                    .resizable()
+                    .scaledToFit()
+                Spacer()
+            }
+            
+            
+            Rectangle()
+                .fill(LinearGradient(
+                    stops: [
+                    Gradient.Stop(color: Color(red: 0.04, green: 0.06, blue: 0.13), location: 0.00),
+                    Gradient.Stop(color: Color(red: 0.04, green: 0.06, blue: 0.13), location: 0.52),
+                    Gradient.Stop(color: Color(red: 0.04, green: 0.06, blue: 0.13).opacity(0), location: 1.00),
+                    ],
+                    startPoint: UnitPoint(x: 0.5, y: 1),
+                    endPoint: UnitPoint(x: 0.5, y: 0)
+                    ))
+            
+            
+        }
     }
 }
 
