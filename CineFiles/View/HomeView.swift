cdb6f252350c962253e94e3013c9b4da6a8e700c
Longhi <72895064+longhirar@users.noreply.github.com>
2025-05-30 09:37:04 -0300

    Update HomeView.swift

--- CineFiles/View/HomeView.swift
+++ CineFiles/View/HomeView.swift
@@ -8,9 +8,56 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    private func movieTab(_ movie: Movie) -> some View {
+        ZStack {
+            VStack {
+                Image(movie.capaArt)
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                Spacer()
+            }
+            
+            Rectangle()
+                .fill(LinearGradient(
+                    stops: [
+                    Gradient.Stop(color: Color(red: 0.04, green: 0.06, blue: 0.13), location: 0.00),
+                    Gradient.Stop(color: Color(red: 0.04, green: 0.06, blue: 0.13), location: 0.52),
+                    Gradient.Stop(color: Color(red: 0.04, green: 0.06, blue: 0.13).opacity(0), location: 1.00),
+                    ],
+                    startPoint: UnitPoint(x: 0.5, y: 1),
+                    endPoint: UnitPoint(x: 0.5, y: 0)
+                    ))
+            
+            VStack {
+                Spacer()
+                Image(movie.capaArt)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 180)
+                    .cornerRadius(12)
+                Text(movie.nome)
+                    .font(.title2)
+                    .lineLimit(2)
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 64)
+                    .frame(maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+            }
+            .frame(width: UIScreen.main.bounds.width, height: .infinity)
+        }
+        .frame(width: UIScreen.main.bounds.width, height: .infinity)
+        .clipped()
+    }
+    
     var body: some View {
         TabView {
-            
+            ForEach(DataModel.movies) { movie in
+                Tab {
+                    movieTab(movie)
+                }
+            }
         }
         .tabViewStyle(.page)
     }
